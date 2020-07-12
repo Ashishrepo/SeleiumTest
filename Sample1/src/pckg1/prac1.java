38ec83bdb9b6ad8643e1672178e7d19b4e19eccc
z0044j3w <z0044j3w@md2et9mc.ad005.onehc.net>
2020-07-12 18:58:58 +0530

    Added a new Project

---
+++ Sample1/src/pckg1/prac1.java
@@ -0,0 +1,13 @@
+
+package pckg1;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class prac1 {
+	
+	  
+	
+	public static void main(String []args) {
+		WebDriver d = new ChromeDriver();
+	}
+}
--- TestGit/src/class1.java
+++ TestGit/src/class1.java
@@ -2,7 +2,10 @@
 public class class1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for(int i=0;i<3;i++) {
+			System.out.println("* " * 3);
+		}
 
 	}
 
